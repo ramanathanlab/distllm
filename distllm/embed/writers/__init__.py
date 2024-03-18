5b88f146242fbc1a4419014c6039b348ef7783f3
braceal <abrace05@gmail.com>
2024-03-18 12:39:42 -0500

    naming

--- distllm/embed/writers/__init__.py
+++ distllm/embed/writers/__init__.py
@@ -11,7 +11,7 @@ from distllm.embed.writers.numpy import NumpyWriter
 from distllm.embed.writers.numpy import NumpyWriterConfig
 from distllm.utils import BaseConfig
 
-WriterConfigs = HuggingFaceWriterConfig | NumpyWriterConfig | None
+WriterConfigs = HuggingFaceWriterConfig | NumpyWriterConfig
 
 STRATEGIES: dict[str, tuple[type[BaseConfig], type[Writer]]] = {
     'huggingface': (HuggingFaceWriterConfig, HuggingFaceWriter),
