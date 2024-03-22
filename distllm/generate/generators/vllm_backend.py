572a3c145c69534436c285740095cef4e98366fc
braceal <abrace05@gmail.com>
2024-03-22 18:27:02 -0500

    bf16

--- distllm/generate/generators/vllm_backend.py
+++ distllm/generate/generators/vllm_backend.py
@@ -39,6 +39,7 @@ class VLLMGenerator:
         config : vLLMGeneratorConfig
             The configuration for the LLMGenerator.
         """
+        import torch
         from vllm import LLM
         from vllm import SamplingParams
 
@@ -61,6 +62,7 @@ class VLLMGenerator:
         self.llm = LLM(
             model=config.llm_name,
             trust_remote_code=config.trust_remote_code,
+            dtype=torch.bfloat16,
         )
 
     def generate(self, prompts: str | list[str]) -> list[str]:
