5b01962b8ada605cb116149706f15017f989a46e
braceal <abrace05@gmail.com>
2024-04-04 21:20:18 -0500

    merge logic

--- distllm/generate/writers/huggingface.py
+++ distllm/generate/writers/huggingface.py
@@ -71,7 +71,15 @@ class HuggingFaceWriter:
             The output directory to write the merged dataset to.
         """
         # Load all the datasets
-        all_datasets = [Dataset.load_from_disk(p) for p in dataset_dirs]
+        all_datasets = []
+        for p in dataset_dirs:
+            # TODO: Debug why for some datasets, we have missing data
+            try:
+                dset = Dataset.load_from_disk(p)
+            except FileNotFoundError:
+                print(f'Skipping dataset {p} as it is missing.')
+                continue
+            all_datasets.append(dset)
 
         # Concatenate the datasets
         dataset = concatenate_datasets(all_datasets)
